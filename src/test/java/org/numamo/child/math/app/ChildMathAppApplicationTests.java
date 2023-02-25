edd33387c3fe124b5649c0805617767d6a743b55
Nazimov Aleksey I <nazimov_alex@mail.ru>
2023-02-26 02:07:58 +0300

    Dev: Fix standar test

--- src/test/java/org/numamo/child/math/app/ChildMathAppApplicationTests.java
+++ src/test/java/org/numamo/child/math/app/ChildMathAppApplicationTests.java
@@ -1,13 +1,20 @@
 package org.numamo.child.math.app;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ChildMathAppApplicationTests {
 
+    @BeforeAll
+    public static void setupHeadlessMode() {
+        System.setProperty("java.awt.headless", "false");
+    }
+
 	@Test
 	void contextLoads() {
 	}
+
 
 }
